56b4dc7c93d337e6dc9ad96af91dcebf04b5faff
Hem Raj Joshi <155612242+coderhem@users.noreply.github.com>
2024-11-04 22:04:01 +0545

    Added Master file

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: "#253E48",
+        },
+        'secondary': {
+          DEFAULT: "#1C2427",
+          100: "#1F617B",
+        },
+        'ss-blue': {
+          DEFAULT: "#6DC4DB ",
+      
+        },
+        'danger': {
+          DEFAULT: '#dc3545',
+        },
+      },
+      screens: {
+        '2xl': '1312px'
+      },
+      fontFamily: {
+        'base': ["Atten New"],
+        'lead': ["ItalianDidotW90-Normal"],
+        'icomoon': ['icomoon'],
+      },
+    },
+  },
+  plugins: [],
+}
