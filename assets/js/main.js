1ee982a4122bcc46e9f50588968248b6ba22f2ec
hemraj <hj130640@gmail.com>
2024-12-31 22:49:35 +0545

    Dashboard updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,63 @@ $('a.slow-scrool[href^="#"]').click(function (e) {
     $('html, body').animate({ scrollTop: target.offset().top }, 1000);
   }
 });
+
+const Button = document.querySelector('#button');
+const Header = document.querySelector('#header')
+
+Button.addEventListener('click', () => {
+  Header.classList.toggle('header-mobile');
+})
+
+// document.addEventListener('click', (event) => {
+//   const select = event.target.closest('.test li');
+//   document.querySelectorAll('.test li').forEach(selectElement => {
+//     if (selectElement === select) {
+//       selectElement.classList.toggle('menulist');
+//     } else {
+//       selectElement.classList.remove('menulist');
+//     }
+//   });
+//  });
+
+const counters = document.querySelectorAll('#number');
+
+const CountUpdate = (counter, duration = 2000) => {
+  const value = parseInt(counter.dataset.value);
+  let initialValue = 0;
+
+  const increment = Math.ceil(value / (duration / 10));
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    if (initialValue >= value) {
+      counter.textContent = `${value}`;
+      clearInterval(increaseCount);
+      return;
+    }
+    counter.textContent = `${initialValue}`;
+  }, 5); // Interval delay
+};
+
+counters.forEach(counter => CountUpdate(counter, 200000000000));
+
+const FormBtn = document.querySelector("#button");
+FormBtn.addEventListener('click', () => {
+  if (FormBtn == null) {
+    FormBtn.classList.add('form-error');
+  }
+  else {
+    FormBtn.classList.add('form-success');
+  }
+})
+
+// const today = new Date().toISOString().split('T')[0];
+// const datePicker = document.getElementById('datePicker');
+// datePicker.setAttribute('min', today);
+
+
+const today = new Date().toISOString().split('T')[0];
+const datePickers = document.querySelectorAll('.datePicker');
+datePickers.forEach(datePicker => {
+  datePicker.setAttribute('min', today);
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,26 +6,44 @@ module.exports = {
     extend: {
       colors: {
         'primary': {
-          DEFAULT: "#253E48",
+          DEFAULT: "#283593",
+          100: '#1d276b',
         },
         'secondary': {
-          DEFAULT: "#1C2427",
-          100: "#1F617B",
+          DEFAULT: "#283593",
+          100 : '#1367c8',
         },
-        'ss-blue': {
+        'blue': {
           DEFAULT: "#6DC4DB ",
-
         },
-        'danger': {
-          DEFAULT: '#dc3545',
+
+        'orange': {
+          DEFAULT: '#ffaa16',
+        },
+
+        'voilet': {
+          DEFAULT: '#673BB7'
+        },
+
+        'red': {
+          DEFAULT: '#ff3939',
+        },
+
+        'green': {
+          DEFAULT: '#198754',
+        },
+        'grey': {
+          DEFAULT: '#F3F3F3',
+          100: '#888888',
+          200 : '#f5f5f5',
         },
       },
       screens: {
         '2xl': '1312px'
       },
       fontFamily: {
-        'base': ["Atten New"],
-        'lead': ["ItalianDidotW90-Normal"],
+        'base': ["Poppins"],
+        'lead': ["Merriweather Sans"],
         'icomoon': ['icomoon'],
       },
     },
